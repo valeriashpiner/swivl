723c554d92982262cb15765b50010e3411d582bb
Valeria Shpiner <shpiner.v@avocadotechnology.com>
2018-04-29 19:18:19 +0300

    Implemet react-native navigation

---
+++ src/containers/enter-screen/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+export default class EnterScreen extends React.Component {
+    render() {
+      return (
+          <Text>EnterScreen</Text>
+      );
+    }
+  }
---
+++ src/router/config.js
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import { StackNavigator } from 'react-navigation';
+
+import EnterScreen from '../containers/enter-screen';
+import FollowerList from '../containers/list-of-followers';
+import UserList from '../containers/list-of-users';
+
+export const Routes = StackNavigator({
+    enterScreen: {
+      screen: EnterScreen
+    },
+    listOfUsers: {
+      screen: UserList
+    },
+    listOfFollowers: {
+       screen: FollowerList
+    },
+  }, {
+    headerMode: 'screen'
+  });
